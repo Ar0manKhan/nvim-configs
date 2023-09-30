5d5fad134f96240034b4913086e553e06d250947
Arman <73093255+Ar0manKhan@users.noreply.github.com>
2023-09-30 07:28:07 +0530

    feat: disabled copilot suggestion

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -3,7 +3,7 @@ local copilot = require("copilot")
 copilot.setup({
 	suggestion = {
 		enabled = true,
-		auto_trigger = true,
+		auto_trigger = false,
 		debounce = 75,
 		keymap = {
 			accept = "<C-a>",
