4f14026d9da2066f77242cf1be52b5364feb47e2
Arman <73093255+Ar0manKhan@users.noreply.github.com>
2023-11-05 18:55:14 +0530

    feat: whichkey plugin

--- lua/arm/packer.lua
+++ lua/arm/packer.lua
@@ -8,15 +8,15 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.3',
-	-- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-	}
+    'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
 
   use({ 'rose-pine/neovim', as = 'rose-pine' })
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
 
   use("theprimeagen/harpoon")
 
@@ -31,10 +31,10 @@ return require('packer').startup(function(use)
   }
 
   use {
-	  "hrsh7th/nvim-cmp",
-	  "hrsh7th/cmp-nvim-lsp",
-	  "saadparwaiz1/cmp_luasnip",
-	  "L3MON4D3/LuaSnip",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
   }
 
   -- action-hints
@@ -49,9 +49,13 @@ return require('packer').startup(function(use)
   use { 'Bekaboo/deadcolumn.nvim' }
 
   use {
-      'numToStr/Comment.nvim',
-      config = function()
-          require('Comment').setup()
-      end
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
+
+  use {
+    "folke/which-key.nvim",
   }
 end)
