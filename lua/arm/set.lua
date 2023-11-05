25fe3a31171b8f4a5a04f24837bea1fa8ea97ac1
Arman <73093255+Ar0manKhan@users.noreply.github.com>
2023-11-05 18:58:22 +0530

    refactor: Vim options in set.lua

--- lua/arm/set.lua
+++ lua/arm/set.lua
@@ -1,30 +1,63 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
+local opt = vim.opt
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+opt.nu = true
+opt.relativenumber = true
 
-vim.opt.wrap = false
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.smartindent = true
+opt.expandtab = false -- Use spaces instead of tabs
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.wrap = false
 
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-vim.opt.termguicolors = true
+opt.hlsearch = true
+opt.incsearch = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+opt.termguicolors = true
 
-vim.opt.updatetime = 50
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
 
-vim.opt.colorcolumn = "80"
+opt.updatetime = 50
 
-vim.g.mapleader = " "
+opt.colorcolumn = "80"
+
+opt.autowrite = true           -- Enable auto write
+opt.clipboard = "unnamedplus"  -- Sync with system clipboard
+opt.completeopt = "menu,menuone,noselect"
+opt.conceallevel = 3           -- Hide * markup for bold and italic
+opt.confirm = true             -- Confirm to save changes before exiting modified buffer
+opt.cursorline = true          -- Enable highlighting of the current line
+opt.formatoptions = "jcroqlnt" -- tcqj
+opt.grepformat = "%f:%l:%c:%m"
+opt.grepprg = "rg --vimgrep"
+opt.ignorecase = true              -- Ignore case
+opt.inccommand = "nosplit"         -- preview incremental substitute
+opt.laststatus = 0
+opt.list = true                    -- Show some invisible characters (tabs...
+opt.mouse = "a"                    -- Enable mouse mode
+opt.number = true                  -- Print line number
+opt.pumblend = 10                  -- Popup blend
+opt.pumheight = 10                 -- Maximum number of entries in a popup
+opt.scrolloff = 5                  -- Lines of context
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize" }
+opt.shiftround = true              -- Round indent
+opt.shortmess:append { W = true, I = true, c = true }
+opt.showmode = false               -- Dont show mode since we have a statusline
+opt.smartcase = true               -- Don't ignore case with capitals
+opt.spelllang = { "en" }
+opt.splitbelow = true              -- Put new windows below current
+opt.splitright = true              -- Put new windows right of current
+opt.timeoutlen = 500               -- speed must be under 500ms inorder for keys to work, increase if you are not able to.
+opt.undolevels = 10000
+opt.wildmode = "longest:full,full" -- Command-line completion mode
+opt.winminwidth = 5                -- Minimum window width
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
